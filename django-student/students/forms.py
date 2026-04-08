34c144759c78246120fce169364aad84f68ec800
Maaaanit <manitcreatives@gmail.com>
2026-04-08 15:12:19 +0530

    Add react-shopping-app-awt-4 lab

--- django-student/students/forms.py
+++ django-student/students/forms.py
@@ -6,9 +6,9 @@ class StudentForm(forms.ModelForm):
         model = Student
         fields = ['name', 'roll_number', 'department', 'semester', 'gmail']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-3 border outline-none'}),
-            'roll_number': forms.TextInput(attrs={'class': 'w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-3 border outline-none'}),
-            'department': forms.TextInput(attrs={'class': 'w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-3 border outline-none'}),
-            'semester': forms.NumberInput(attrs={'class': 'w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-3 border outline-none'}),
-            'gmail': forms.EmailInput(attrs={'class': 'w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-3 border outline-none'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'roll_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'department': forms.TextInput(attrs={'class': 'form-control'}),
+            'semester': forms.NumberInput(attrs={'class': 'form-control'}),
+            'gmail': forms.EmailInput(attrs={'class': 'form-control'}),
         }
